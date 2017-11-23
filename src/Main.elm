cd8447a297029a9cd878e0db11fb1cf219263d21
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-11-23 08:53:13 +0100

    Upgrade to Html.program

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,41 @@ import Html exposing (node, text)
 import Html.Attributes exposing (attribute)
 
 
+main : Program Never Model Msg
 main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    ()
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd msg )
+init =
+    () ! []
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    () ! []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions =
+    always Sub.none
+
+
+view : Model -> Html.Html Msg
+view model =
     Html.node "ons-button"
         [ attribute "onClick" "window.alert('Hello, World!')" ]
         [ text "Click Me" ]
